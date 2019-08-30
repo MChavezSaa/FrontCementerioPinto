4ebb9236f2214dbb74abef74cf65d16575b57bf4
MChavezSaa <matroxmega@gmail.com>
2019-08-30 00:37:41 -0400

    PATIO G listo

--- src/app/cliente/cliente-content/c-mostrar-terrenos/c-mostrar-terrenos.component.ts
+++ src/app/cliente/cliente-content/c-mostrar-terrenos/c-mostrar-terrenos.component.ts
@@ -122,13 +122,6 @@ export class CMostrarTerrenosComponent implements OnInit {
       "id_tumba" : "16" ,
       "name": "lalal  Mueller",
       "FD": "26/6/2019"
-    },
-    {
-
-      "id_terreno ": "1" ,
-      "id_tumba" : "16" ,
-      "name": "lalal  Mueller",
-      "FD": "26/6/2019"
     }
     
   ];
@@ -447,6 +440,178 @@ export class CMostrarTerrenosComponent implements OnInit {
     }
   ];
     
+  peopleG: any[] = [
+    {      
+      "id_terreno ": "7" ,
+      "id_tumba" : "1" ,
+      "name": "Douglas  Pace",
+      "FD": "26/6/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "2" ,
+      "name": "Mcleod  Mueller",
+      "FD": "26/7/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "3" ,
+      "name": "lalal  Mueller",
+      "FD": "26/6/2019"
+    },
+    {      
+      "id_terreno ": "7" ,
+      "id_tumba" : "4" ,
+      "name": "Douglas  Pace",
+      "FD": "26/6/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "5" ,
+      "name": "Mcleod  Mueller",
+      "FD": "26/7/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "6" ,
+      "name": "lalal  Mueller",
+      "FD": "26/6/2019"
+    },
+    {      
+      "id_terreno ": "7" ,
+      "id_tumba" : "7" ,
+      "name": "Douglas  Pace",
+      "FD": "26/6/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "8" ,
+      "name": "Mcleod  Mueller",
+      "FD": "26/7/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "9" ,
+      "name": "lalal  Mueller",
+      "FD": "26/6/2019"
+    },
+    {      
+      "id_terreno ": "7" ,
+      "id_tumba" : "10" ,
+      "name": "Douglas  Pace",
+      "FD": "26/6/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "11" ,
+      "name": "Mcleod  Mueller",
+      "FD": "26/7/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "12" ,
+      "name": "lalal  Mueller",
+      "FD": "26/6/2019"
+    },
+    {      
+      "id_terreno ": "7" ,
+      "id_tumba" : "13" ,
+      "name": "Douglas  Pace",
+      "FD": "26/6/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "14" ,
+      "name": "Mcleod  Mueller",
+      "FD": "26/7/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "15" ,
+      "name": "lalal  Mueller",
+      "FD": "26/6/2019"
+    },
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "16" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }, 
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "17" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    },     
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "18" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }, 
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "19" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }, 
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "20" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }, 
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "21" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }, 
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "22" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }, 
+    {
+
+      "id_terreno ": "7" ,
+      "id_tumba" : "23" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    },
+    {
+      "id_terreno ": "7" ,
+      "id_tumba" : "24" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    },
+    {
+      "id_terreno ": "7" ,
+      "id_tumba" : "25" ,
+      "name": "maty",
+      "FD": "26/6/2019"
+    }
+    
+  ];
+
   guardarID(iD: number){
     //para poder cargar el nombre y fecha defuncion necesito id de la tumba buscada aqui se guarda ese id
     //console.log("id: "+ iD);    
