363021e18683dd846b366095019ae5942d943b53
KoniBelen <konibelen12@gmail.com>
2019-10-09 19:24:10 -0300

    Update backend-service.service.ts

--- src/app/Service/backend-service.service.ts
+++ src/app/Service/backend-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cliente } from '../empleado/empleado-content/e-clientes/cliente';
 
@@ -10,6 +10,8 @@ import { Cliente } from '../empleado/empleado-content/e-clientes/cliente';
 export class BackendServiceService {
 
   private urlEndPoint:string= 'http://localhost:8080/';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'}); 
+
   constructor(private http:HttpClient) { }
 
 
