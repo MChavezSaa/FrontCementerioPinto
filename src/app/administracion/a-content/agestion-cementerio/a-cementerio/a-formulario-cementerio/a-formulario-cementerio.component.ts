267e1e841399f93c0d1aff61fc35cca32861b333
MChavezSaa <matroxmega@gmail.com>
2019-10-17 14:31:56 -0300

    save de cementerio enlazado con backend

--- src/app/administracion/a-content/agestion-cementerio/a-cementerio/a-formulario-cementerio/a-formulario-cementerio.component.ts
+++ src/app/administracion/a-content/agestion-cementerio/a-cementerio/a-formulario-cementerio/a-formulario-cementerio.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendServiceService } from 'src/app/Service/backend-service.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2' ;
+import { Router } from '@angular/router';
+import { Cementerio } from 'src/app/Entidades/Cementerio';
+
+
 
 @Component({
   selector: 'app-a-formulario-cementerio',
@@ -7,9 +14,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AFormularioCementerioComponent implements OnInit {
 
-  constructor() { }
+  formCementerio: FormGroup;
+
+  constructor(private service: BackendServiceService, private formBuilder: FormBuilder,
+    private router:Router,
+
+    ) {
+
+    this.formCementerio = this.formBuilder.group({
+      nombre_Cementerio: ['', [Validators.required]] ,
+      direccion_Cementerio: ['', [Validators.required]],
+      telefono_Cementerio:['', [Validators.required]],
+      capacidad_Terrenos: ['', [Validators.required]]
+  });    
+   }
 
   ngOnInit() {
+  }
+
+
+  private cementerio: Cementerio;
+  public llenarCementerio(){
+    
+  }
+
+  public create():void{
+    this.service.saveCementerio(this.formCementerio.value)
+      .subscribe(
+      cementerio => {   
+        //ver como tomar valor de nombre para funcion swal
+        Swal.fire('Nuevo cementerio', `Cementerio ${document.getElementById("nombre_Cementerio")} creado con Exito`, 'success');    
+        this.router.navigate(['/administracion-inicio/ACementerio']);  
+      },
+      err=>{
+        console.log(err)
+      }
+    );
   }
 
 }
