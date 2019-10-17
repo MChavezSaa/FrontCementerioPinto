c54c430aac2cb555e5bb09ad581a37fe4585ffc7
MChavezSaa <matroxmega@gmail.com>
2019-10-17 16:24:59 -0300

    cargar lista en option

--- src/app/Entidades/Terreno.ts
+++ src/app/Entidades/Terreno.ts
@@ -2,6 +2,7 @@ import { Cementerio } from './Cementerio';
 
 export interface Terreno {
     id_terreno: number;
+    nombre_Terreno:String;
     capacidad_Terreno: number;
     estado_Terreno: boolean;
     cementerio: Cementerio;
--- src/app/administracion/a-content/agestion-cementerio/a-terreno/a-formulario-terreno/a-formulario-terreno.component.ts
+++ src/app/administracion/a-content/agestion-cementerio/a-terreno/a-formulario-terreno/a-formulario-terreno.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Cementerio } from 'src/app/Entidades/Cementerio';
+import { BackendServiceService } from 'src/app/Service/backend-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2' ;
+
 
 @Component({
   selector: 'app-a-formulario-terreno',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./a-formulario-terreno.component.css']
 })
 export class AFormularioTerrenoComponent implements OnInit {
+  
+  cementerioList:Cementerio[] = [];
+  formTerreno: FormGroup;
 
-  constructor() { }
+
+  constructor(private service: BackendServiceService, private formBuilder: FormBuilder,
+    private router:Router) {
+      this.formTerreno = this.formBuilder.group({
+        nombre_Terreno: ['', [Validators.required]] ,
+        capacidad_Terreno: ['', [Validators.required]] ,
+        estado_Terreno: ['', [Validators.required]],
+        cementerio:['', [Validators.required]]
+    });     
+    }
 
   ngOnInit() {
+    this.service.getCementerio().subscribe(cementerioList1 => this.cementerioList = cementerioList1);
+  }
+
+  public create():void{
+    this.service.saveCementerio(this.formTerreno.value)
+      .subscribe(
+      terreno => {   
+        //ver como tomar valor de nombre para funcion swal
+        Swal.fire('Nuevo Terreno', `Terreno ${terreno} creado con Exito`, 'success');    
+        this.router.navigate(['/administracion-inicio/ACementerio']);  
+      },
+      err=>{
+        console.log(err)
+      }
+    );
   }
 
 }
