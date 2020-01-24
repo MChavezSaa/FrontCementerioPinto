ec2a07b3da36c83e4746a3ddc9cc204544cf91d9
KoniBelen <konibelen12@gmail.com>
2020-01-24 13:55:44 -0300

    Cambio en home barra superior, agrega contrato2

---
+++ src/app/Entidades/ContratoDos.ts
@@ -0,0 +1,28 @@
+import { Cementerio } from './Cementerio';
+import { Terreno } from './Terreno';
+import { Patio } from './Patio';
+import { Tumba } from './Tumba';
+import { TipoTumba } from './TipoTumba';
+import { Derecho } from './Derecho';
+import { Cliente } from './Cliente';
+import { Funcionario } from './Funcionario';
+
+export class ContratoDos{
+    id_contrato2: number;
+    fecha_Ingreso_Venta: Date;
+    fecha_Pago: Date;
+    medio_Pago:string;
+    valor_Terreno: number;
+    pagoInicial:number;
+    n_Cuotas: number;
+    VCuotas: number;
+    
+    cementerio: Cementerio;
+    terreno: Terreno;
+    patio: Patio;
+    tumba: Tumba;
+    tipoTumba: TipoTumba;
+    cliente: Cliente;
+    funcionario:Funcionario;
+    derecho: Derecho;
+}
